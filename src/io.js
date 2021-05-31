abe767a70cb132dac72aa1953653cc96b2bdb012
Merlijn van der Kamp <zemanzo.my@gmail.com>
2021-05-31 21:59:46 +0200

    Moved type checking to server; Added endpoint for image files only

--- src/io.js
+++ src/io.js
@@ -3,21 +3,43 @@ const path = require("path");
 const config = require("../config.json");
 const ROOT_DIRECTORY = path.resolve(config.root);
 
+const _hasImageExtension = name => {
+  return [".jpg", ".jpeg", ".png", ".gif", "webp"].some((ext) =>
+    name.endsWith(ext)
+  );
+};
+
+const _getEntryType = entry => {
+    if (entry.isDirectory()) {
+      return "folder";
+    } else if (_hasImageExtension(entry.name)) {
+      return "image";
+    }
+    return "file";
+}
+
 const getDirectoryContents = async (directory) => {
   const dir = path.join(ROOT_DIRECTORY, directory);
+
   if (!dir.includes(ROOT_DIRECTORY)) {
     throw new Error("The requested directory is higher than the root directory, which is not allowed.");
   }
+
   const entries = (await fs.readdir(dir, { withFileTypes: true }))
     .map(entry => {
       return {
         name: entry.name,
-        type: entry.isDirectory() ? "folder" : "file"
+        type: _getEntryType(entry)
       };
     });
   return entries;
 }
 
+const getDirectoryImages = async (directory) => {
+  return (await getDirectoryContents(directory)).filter(entry => entry.type === "image");
+}
+
 module.exports = {
   getDirectoryContents,
+  getDirectoryImages
 };
--- src/routes/directory/index.js
+++ src/routes/directory/index.js
@@ -18,6 +18,24 @@ async function routes(fastify, options) {
         });
     }
   });
+
+  fastify.get("/api/directoryImages", async (req, res) => {
+    const { path } = req.query;
+    try {
+      const contents = await io.getDirectoryImages(path);
+      res
+        .code(200)
+        .header("Content-Type", "application/json; charset=utf-8")
+        .send(contents);
+    } catch (err) {
+      return res
+        .code(400)
+        .header("Content-Type", "application/json; charset=utf-8")
+        .send({
+          err,
+        });
+    }
+  });
 }
 
 module.exports = routes;
